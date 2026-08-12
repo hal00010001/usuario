0b863fcf7fd9d1026dbf7d00a5df234048e32ee2
Almir L. Bindo <hal0001@gmail.com>
2026-08-12 01:07:36 -0300

    security added

---
+++ src/main/java/br/com/ambidextrous/usuario/entity/Endereco.java
@@ -0,0 +1,32 @@
+package br.com.ambidextrous.usuario.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String logradouro;
+    private String numero;
+    @Column(length = 100)
+    private String complemento;
+    private String bairro;
+    @Column(length = 100)
+    private String cidade;
+    @Column(length = 2)
+    private String uf;
+    @Column(length = 9)
+    private String cep;
+
+}
---
+++ src/main/java/br/com/ambidextrous/usuario/entity/Telefone.java
@@ -0,0 +1,25 @@
+package br.com.ambidextrous.usuario.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "telefone")
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 10)
+    private String numero;
+    @Column(length = 3)
+    private String ddd;
+
+}
